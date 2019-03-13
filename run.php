cd0f6b1a42ed264d17d32b47096891b88ee2bee1
BANKA2017 <kdwnil@hotmail.com>
2019-03-13 14:02:02 +0800

    修复无法签到的问题

--- run.php
+++ run.php
@@ -125,6 +125,7 @@ class Acsign
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, array("access_token" => $this->access_token));
             curl_setopt($ch,CURLOPT_ENCODING , "gzip");
+            curl_setopt($ch,CURLOPT_HTTPHEADER,["acPlatform: ANDROID_PHONE", "appVersion: 5.14.0.655"]);
             $sign = json_decode(curl_exec($ch) , true);
             curl_close($ch);
             //if($sign["result"] == -401){//token失效的时候我也很绝望啊
@@ -146,6 +147,7 @@ class Acsign
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, array("access_token" => $this->access_token));
             curl_setopt($ch,CURLOPT_ENCODING , "gzip");
+            curl_setopt($ch,CURLOPT_HTTPHEADER,["acPlatform: ANDROID_PHONE", "appVersion: 5.14.0.655"]);
             $sign = json_decode(curl_exec($ch) , true);
             curl_close($ch);
             if(!$sign["hasSignedIn"]){//token失效的时候我也很绝望啊
