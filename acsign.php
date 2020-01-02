6b67292dddb68052ec96ddc6c452d14ec08272cf
BANKA2017 <kdwnil@hotmail.com>
2020-01-02 18:25:10 +0800

    fix: 修复acPasstoken的大小写问题 rewrite: 重写Acsign类

--- acsign.php
+++ acsign.php
@@ -1,72 +1,49 @@
 <?php
-
 /**
  * Author: BANKA2017
- * Version: 4.2
+ * Version: 5.0
  */
 class Acsign{
-    public $username, $password, $date, $access_token, $auth_key, $acPasstoken;
-    private $ch;
-    private function scurl($url) {
-        $this -> ch = curl_init();
-        curl_setopt($this -> ch, CURLOPT_URL, $url);
-        curl_setopt($this -> ch, CURLOPT_USERAGENT, 'acvideo core/6.11.1.822');
-        curl_setopt($this -> ch, CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($this -> ch, CURLOPT_RETURNTRANSFER, true);
-        return $this;
-    }
-
-    /*获取日期*/
-    public function get_date() {
-        return date("Ymd");
+    public $username, $password, $access_token, $auth_key, $acPasstoken;
+    private function scurl(string $url = "localhost", string $type = "get", array $header = [], array $data = []): array {
+        $ch = curl_init();
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => "acvideo core/6.11.1.822",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_HTTPHEADER => $header,
+        ];
+        if(strtolower($type) == "post"){
+            $options[CURLOPT_POST] = true;
+            $options[CURLOPT_POSTFIELDS] = http_build_query($data);
+        }
+        curl_setopt_array($ch, $options);
+        return json_decode(curl_exec($ch), true);
     }
 
     /*客户端登录*/
-    public function mo_login() {
-        self::scurl('https://id.app.acfun.cn/rest/app/login/signin');
-        curl_setopt($this -> ch, CURLOPT_POST, true);
-        curl_setopt($this -> ch, CURLOPT_POSTFIELDS, http_build_query(["username" => $this -> username, "password" => $this -> password]));
-        curl_setopt($this -> ch, CURLOPT_HTTPHEADER, ["deviceType: 1"]);
-        $json = json_decode(curl_exec($this -> ch), true);
-        if(!$json["result"]){
-            $this->access_token = $json["token"];
-            $this->acPasstoken = $json["acPassToken"];
-            $this->auth_key = $json["auth_key"];
+    public function mo_login(): bool {
+        $loginResult = self::scurl("https://id.app.acfun.cn/rest/app/login/signin", "post", ["deviceType: 1"], ["username" => $this->username, "password" => $this->password]);
+        if(!($loginResult["result"]??true)){
+            $this->access_token = $loginResult["token"];
+            $this->acPasstoken = $loginResult["acPassToken"];
+            $this->auth_key = $loginResult["auth_key"];
         }
-        curl_close($this -> ch);
-        return $json["result"];
-    }
-
-    /*客户端签到接口*/
-    public function mo_nsign()
-    {
-        if ($this->get_date() > $this -> date) {
-            self::scurl('https://api-new.acfunchina.com/rest/app/user/signIn');
-            curl_setopt($this -> ch, CURLOPT_POST, true);
-            curl_setopt($this -> ch, CURLOPT_POSTFIELDS, http_build_query(["access_token" => $this->access_token]));
-            curl_setopt($this -> ch, CURLOPT_HTTPHEADER, ["acPlatform: ANDROID_PHONE", "Cookie: auth_key={$this->auth_key};acPasstoken={$this->acPasstoken}"]);
-            $sign = json_decode(curl_exec($this -> ch), true);
-            curl_close($this -> ch);
-            $this->signed_date = $this->get_date();
-            return $sign["msg"];
-        } else {
-            return "今日已签到";
-        }
+        return (bool)$loginResult["result"]??false;
     }
 
     /*客户端检查签到接口*/
-    public function c_sign() {
-        self::scurl('https://api-new.acfunchina.com/rest/app/user/hasSignedIn');
-        curl_setopt($this -> ch, CURLOPT_POST, true);
-        curl_setopt($this -> ch, CURLOPT_POSTFIELDS, http_build_query(["access_token" => $this->access_token]));
-        curl_setopt($this -> ch, CURLOPT_HTTPHEADER, ["acPlatform: ANDROID_PHONE", "Cookie: auth_key={$this->auth_key};acPasstoken={$this->acPasstoken}"]);
-        $sign = json_decode(curl_exec($this -> ch), true);
-        curl_close($this -> ch);
-        return $sign["hasSignedIn"] ?? false;
+    public function c_sign(): array {
+        $checkSign = self::scurl("https://api-new.acfunchina.com/rest/app/user/hasSignedIn", "post", ["acPlatform: ANDROID_PHONE", "Cookie: auth_key={$this->auth_key};acPasstoken={$this->acPasstoken}"], ["access_token" => $this->access_token]);
+        return ["result" => $checkSign["result"] ?? -401, "hasSignedIn" => $checkSign["hasSignedIn"] ?? false];
     }
 
-    /*显示*/
-    public function display() {
-        echo $this->username . " -> sign:" . self::mo_nsign() . "\n";
+    /*客户端签到接口*/
+    public function mo_nsign(): string {
+        $signInfo = self::scurl("https://api-new.acfunchina.com/rest/app/user/signIn", "post", ["acPlatform: ANDROID_PHONE", "Cookie: auth_key={$this->auth_key};acPasstoken={$this->acPasstoken}"], ["access_token" => $this->access_token]);
+        return $signInfo["msg"] ?? "网络请求失败";
     }
 }
--- run.php
+++ run.php
@@ -1,61 +1,72 @@
 <?php
 /**
  * Author: BANKA2017
- * Version: 4.1
+ * Version: 5.0
  */
 header('Content-Type: text/txt; charset=UTF-8');
 set_time_limit(0);
 ignore_user_abort(true);
-require(dirname(__FILE__) . "/acsign.php");
+require(__DIR__ . '/acsign.php');
 echo "Acsign\n";
 
-PHP_SAPI == "cli" ? define('IS_CLI',true) : define('IS_CLI',false);
+$ut = -1;//usertype
+$userInfo = [];
+$dataTemplate = [
+    "status" => 1,
+    "date" => 0,
+    "account" => [
+        "username" => "",
+        "password" => ""
+    ],
+    "token" => [
+        "access_token" => "",
+        "acPasstoken" => "",
+        "auth_key" => ""
+    ]
+];
 
-if(file_exists(dirname(__FILE__) . '/task.json')){
-    $data = $data = json_decode(file_get_contents(dirname(__FILE__) . '/task.json') , true);
-    $ut = 0;//usertype
-}elseif(IS_CLI){
+if(file_exists(__DIR__ . '/task.json')){
+    $userInfo = json_decode(file_get_contents(__DIR__ . '/task.json'), true);
+    $ut = 0;
+}elseif((isset($argv[1]) && isset($argv[2])) || (isset($_REQUEST["username"]) && isset($_REQUEST["password"]))){
     //TODO 多用户
-    if(isset($argv[1]) && isset($argv[2])){
-        $data = [["status" => 1,"date" => 0,"account" => ["username" => $argv[1], "password" => $argv[2]], "cookie" => ["access_token" => "", "acPasstoken" => "", "auth_key" => ""]]];
-        $ut = 1;
-    }else{
-        die("Acsign : 缺少有效参数\n");
-    }
-}elseif(isset($_REQUEST["username"]) && isset($_REQUEST["password"])){
-    $data = [["status" => 1, "date" => 0, "account" => ["username" => $_REQUEST["username"], "password" => $_REQUEST["password"]], "cookie" => ["access_token" => "","acPasstoken" => "","auth_key" => ""]]];
-    $ut = 2;
+    $userInfo[] = $dataTemplate;
+    $userInfo[0]["account"]["username"] = $argv[1]??$_REQUEST["username"];
+    $userInfo[0]["account"]["password"] = $argv[2]??$_REQUEST["username"];
+    $ut = 1;
 }else{
-    die("Acsign : 缺少有效参数\n");
+    die("Acsign : 缺少有效参数或文件\n");
 }
+
 $sign = new Acsign;
-foreach($data as $key => $value){
-    //检查登录情况
-    if($value["status"] && $value["date"] < $sign -> get_date()){
+foreach($userInfo as $order => $value){
+    if($value["status"] && $value["date"] < date("Ymd")){
         $sign->username = $value["account"]["username"];
         $sign->password = $value["account"]["password"];
-        $sign->access_token = $value["cookie"]["access_token"];
-        $sign->acPasstoken = $value["cookie"]["acPassToken"];
-        $sign->auth_key = $value["cookie"]["auth_key"];
-        $login_state = false;
-        if(!$sign -> access_token || !$sign -> c_sign()){
-            if($sign -> username && $sign -> password){
-                $login_state = $sign -> mo_login();
+        $sign->access_token = $value["token"]["access_token"]??null;
+        $sign->acPasstoken = $value["token"]["acPasstoken"]??null;
+        $sign->auth_key = $value["token"]["auth_key"]??null;
+        $checkSign = $sign->c_sign();
+        if($checkSign["result"] == -401){
+            if((bool)$sign->username && (bool)$sign->password){
+                $checkSign["result"] = (int)$sign->mo_login();
             }else{
-                $login_state = false;
+                $checkSign["result"] = 1;
             }
         }
-        if($login_state){
-            $sign -> display();
+        if(!$checkSign["result"]){
+            echo "[{$order}]" . $sign->username . " -> sign:" . $sign->mo_nsign() . "\n";
             if(!$ut){
-                $data[$key]["cookie"]["access_token"] = $sign -> access_token;
-                $data[$key]["cookie"]["acPassToken"] = $sign -> acPassToken;
-                $data[$key]["cookie"]["auth_key"] = $sign -> auth_key;
-                $data[$key]["date"] = $sign -> signed_date;
+                $userInfo[$order]["token"]["access_token"] = $sign->access_token;
+                $userInfo[$order]["token"]["acPasstoken"] = $sign->acPasstoken;
+                $userInfo[$order]["token"]["auth_key"] = $sign->auth_key;
+                $userInfo[$order]["date"] = date("Ymd");
             }
         }
+    }else{
+        echo "[{$order}]{$value["account"]["username"]} -> 已跳过\n";
     }
 }
 if(!$ut){
-    file_put_contents(dirname(__FILE__) . '/task.json', json_encode($data,JSON_UNESCAPED_UNICODE));
+    file_put_contents(__DIR__ . '/task.json', json_encode($userInfo, JSON_UNESCAPED_UNICODE));
 }
